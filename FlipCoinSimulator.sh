2e9edd92cab9c5dc049af98ae2e9e9bd8e7b7f78
IamSanketMore <sanketmore.999.m@gmail.com>
2020-12-18 21:08:09 +0530

    Add Flip Coin Simulation UC1 Problem to UC1 Branch

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,1 +1,11 @@
-echo "Add File to main"
+#!/bin/bash
+
+Result=$((RANDOM%2))
+if [[ ${Result} -eq 0 ]]
+then
+    echo "Coin Flip and Winner is Head"
+elif [[ ${Result} -eq 1 ]]
+then
+    echo "Coin Flip and Winner is Tail"
+fi
+
