b38d1bb4ab01f026559a8716085aa421158b77f5
IamSanketMore <sanketmore.999.m@gmail.com>
2020-12-19 00:20:03 +0530

    Add Flip Coin Simulation UC3 Problem to UC3 Branch

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -3,17 +3,30 @@
 head=0
 tail=0
 
-for (( i = 1; i<=10; i++ ))
+while [ $head -lt 21 ] && [ $tail -lt 21 ]
 do
-	Result[$i]=$((RANDOM%2))
+	Result=$(( RANDOM % 2 ))
 
-	if [[ ${Result[$i]} -eq 1 ]]
+	if [[ $Result -eq 1 ]]
 	then
 		head=$(( $head + 1 ))
 	else
 		tail=$(( $head +1 ))
 	fi
 done
-echo ${Result[*]}
-echo "Head has Won $head Times"
-echo "Tail has Won $tail Times"
+if [[ $head -eq 21 && $tail -eq 21 ]]
+then
+	echo "Its a Tie Head And Tail Occured 21 Times "
+
+	elif [ $head -eq 21 ]
+	then
+		echo "Head is Won And Firstly Occured 21 Times"
+		head_Diff=$(( $head - $tail ))
+		echo "Head is:- $head And Tail is:- $tail "
+		echo "Head is Won By $head_Diff"
+	else
+		echo "Tail is Won And Firstly Occured 21 Times"
+		tail_Diff=$(( $tail - $head ))
+		echo "Head is:- $head And Tail is:- $tail "
+		echo "Tail is Won By $tail_Diff"
+fi
